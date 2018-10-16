52b2d3248b71d65acc40965c18f91e843f3b8c7a
thomas <thomas.bellec.tb@gmail.com>
2018-10-16 15:17:39 +0200

    Issue: 5 images per line on home page fixed

--- series/APIClient.py
+++ series/APIClient.py
@@ -1,6 +1,23 @@
 """API Client for the The Movie DB api
 All functions are stored here and API calls made with the API key stored in settings.py"""
 
+"""
+Je veux :
+- image
+- noms des acteurs
+- réalisateur
+- pays
+- duration
+- categories (drame, comedy...)
+- date de sortie
+- rating
+- synopsis
+- saisons et épisodes       
+- reviews   
+- Others ?
+- id
+
+"""
 from django.conf import settings
 import requests
 
@@ -24,3 +41,48 @@ class APIClient:
 
         result = self.call('GET', url)
         return result
+
+    def get_tv_show_details(self, tv_id):
+        url = 'https://api.themoviedb.org/3/tv/' + str(tv_id)
+
+        details = self.call('GET', url)
+        real = []
+        if details['created_by'] == []:
+            real = ['non renseigné']
+        else:
+            for realisateur in details['created_by']:
+                real.append(realisateur['name'])
+
+
+        details_useful = {'realisateur': real,
+                          'name': details['name'],
+                          'number_of_episodes': details['number_of_episodes'],
+                          'origin_country': details['origin_country'][0],
+                          'language': details['original_language'],
+                          'popularity': details['popularity'],
+                          'poster': 'https://image.tmdb.org/t/p/w500' + details['poster_path'],
+                          'first_air_date': details['first_air_date'],
+                          'next_episode_to_air': details['next_episode_to_air'],
+                          }
+
+        return details_useful
+
+    def get_tv_show_cast(self, tv_id, season_number = 1):
+        url = 'https://api.themoviedb.org/3/tv/' + str(tv_id) + '/season/' +str(season_number) +'/credits'
+
+        details = self.call('GET', url)
+        actors = details['cast']
+        crew = details['crew']
+        cast = {'actors': actors, 'crew': crew}
+        for actor in cast['actors']:
+            actor['profile_path'] = 'https://image.tmdb.org/t/p/w200' + str(actor['profile_path'])
+        return cast
+
+    def get_tv_shows_reviews(self, tv_id):
+        url = 'https://api.themoviedb.org/3/tv/' + str(tv_id) + '/reviews'
+
+        reviews = self.call('GET', url)
+        if reviews['results'] == []:
+            return [{'author': "pas de reviews pour cette série", 'content': ""}]
+        else:
+            return reviews['results'] # return a list of reviews contained in dictionaries
