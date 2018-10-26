bfb42dcd34e2b592b12ebbb42d57cce32063d171
thomas <thomas.bellec.tb@gmail.com>
2018-10-26 11:57:30 +0200

    FAVORIIIITE

--- user/ThreadFavorites.py
+++ user/ThreadFavorites.py
@@ -1,4 +1,5 @@
 from threading import Thread, RLock, BoundedSemaphore
+from dbtables.Favorite import Favorite
 import random
 import time
 
@@ -13,6 +14,13 @@ class ThreadFavorites(Thread):
 
 
 def get_user_favorites(favorites):
+    """
+    :param favorites: list of objects favorite from the class Favorite
+    :type favorites: oject
+    """
+    favorite_test = Favorite(1, 1402)
+    favorite_test_2 = Favorite(1, 60735)
+    favorite_test_bobby = Favorite(2, 1418)
     Threads = []
     for favorite in favorites:
         Threads.append(ThreadFavorites(favorite))
@@ -20,3 +28,7 @@ def get_user_favorites(favorites):
         Threads[k].start()
     for l in range(0, len(Threads)):
         Threads[l].join()
+    Threads.append(favorite_test.get_favorite_details())
+    Threads.append(favorite_test_2.get_favorite_details())
+    Threads.append(favorite_test_bobby.get_favorite_details())
+    return Threads
