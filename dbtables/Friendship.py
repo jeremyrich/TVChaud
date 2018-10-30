6c50917e6ed483a354f4ff43449c034b88cf17fa
thomas <thomas.bellec.tb@gmail.com>
2018-10-30 15:52:41 +0100

    FRIENDS, MARTIFRIENDS !

--- dbtables/Friendship.py
+++ dbtables/Friendship.py
@@ -2,3 +2,5 @@ class Friendship:
     def __init__(self, user_id_1, user_id_2):
         self.__user_id_1 = user_id_1
         self.__user_id_2 = user_id_2
+
+
--- user/views.py
+++ user/views.py
@@ -3,16 +3,19 @@ from django.contrib.auth.decorators import login_required
 import user.ThreadFavorites as ThreadFavorites
 from dbtables.User import User
 # Create your views here.
+from django.contrib.auth.models import User as djangoUser
 
 @login_required
 def user_details(request, user_id):
-    user = User(request)
+    user = User(request.user.id, request.user.username, request.user.password)
+    friends = user.get_friends()
     # Retourne une liste d'objets Favorite
     favorites = user.get_my_favorites()
     # on appelle le multithreading depuis les objets Favorites
     favorites_details = ThreadFavorites.get_user_favorites(favorites)
     output = {'user_id': user_id,
               'user': user,
-              'details': favorites_details}
-
+              'details': favorites_details,
+              'friends': friends
+              }
     return render(request, 'user/user_details.html', output)
