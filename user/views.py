d642ff0d339929ede44fe2d36f5e6915f0177d6e
hugomartinet <hugo.martinet@gmail.com>
2018-11-05 15:44:18 +0100

    a few things

--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 from user.FavoriteThread import get_user_favorites
+from user.NotifThread import load_notifications
 from dbtables.User import User
 from dbtables.Favorite import Favorite
 
@@ -15,6 +16,8 @@ def user_details(request, user_id):
     intermediate = djangoUser.objects.get(id=user_id)
     user = User(intermediate.id, intermediate.username, intermediate.password)
     friends = user.get_friends()
+
+    notifs = load_notifications(request)
 
     # On appelle le multithreading pour obtenir les favorites du user
     favorites = get_user_favorites(user)
